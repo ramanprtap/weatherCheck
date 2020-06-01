a5cab52e4e3dfbe0bbe2c750b42418b837b79428
raman pratap <ramanprtap>
2020-06-01 19:09:19 +0530

    Updates

---
+++ precache-manifest.63871c7d6d3de6a3e7ba0bd27a0e02b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32136bdfddab50321cf40d0b741db9b",
+    "url": "/ramanprtap/weatherCheck.git/index.html"
+  },
+  {
+    "revision": "7d39ef3f383fb6838327",
+    "url": "/ramanprtap/weatherCheck.git/static/css/main.4c040b22.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/ramanprtap/weatherCheck.git/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ramanprtap/weatherCheck.git/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d39ef3f383fb6838327",
+    "url": "/ramanprtap/weatherCheck.git/static/js/main.77cc9f0c.chunk.js"
+  },
+  {
+    "revision": "4b9c0c254da728d9f13d",
+    "url": "/ramanprtap/weatherCheck.git/static/js/runtime-main.05b747b3.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/ramanprtap/weatherCheck.git/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/ramanprtap/weatherCheck.git/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
